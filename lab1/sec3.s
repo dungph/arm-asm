e929f5ed6e6c795dfe52485d1c1744348f122bf3
Phạm Văn Dũng <dung18j@gmail.com>
2022-09-25 20:28:33 +0700

    init

---
+++ lab1/sec3.s
@@ -0,0 +1,12 @@
+;n!
+    area reset, data, readonly
+        dcd 0x20001000
+        dcd reset
+
+    area main, code, readonly
+        entry
+        export reset
+
+reset 
+    
+    end
---
+++ lab1/sec5.s
@@ -0,0 +1,19 @@
+sum 1...n
+sum odd, even 1...n
+n!
+sum 1...n mod 5 = 0
+x^n
+sum 1 + x^2 + x^3 + ... + x^n
+gcd
+lcm
+
+    area reset, data, readonly
+        dcd 0x20001000
+        dcd reset
+
+    area main, code, readonly
+        entry
+        export reset
+
+reset 
+    end
