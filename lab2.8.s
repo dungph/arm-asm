7ce0a669fd56aab8e0d2fbd84bb2b25af57b998c
Phạm Văn Dũng <dung18j@gmail.com>
2022-10-02 10:54:13 +0700

    ahh

--- lab2.8.s
+++ lab2.8.s
@@ -1,6 +1,3 @@
-; ;1.Viết chương trình tính tổng các phần tử trong chuỗi số.
-; ;2.Viết chương trình tìm giá trị lớn nhất và nhỏ nhất trong chuỗi số.
-; ;3.Viết chương trình tính trung bình cộng của chuỗi số.
 ; ;4.Viết chương trình tính tổng các phần tử trong ma trận
 ; ;5.Viết chương trình đếm số phần tử dương, âm trong ma trận
 ; ;6.Viết chương trình tìm giá trị lớn nhất, nhỏ nhất trên đường chéo chính, chéo phụ
@@ -12,39 +9,79 @@
         ; dcd reset_handler
 
 ; nums
-    ; dcd 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-    ; dcd 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-    ; dcd 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-    ; dcd 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-    ; dcd 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-    ; dcd 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-    ; dcd 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-    ; dcd 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-    ; dcd 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-    ; dcd 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-    ; dcd 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+    ; dcd 40, 1
+	; dcd 2, 5
 
+
+; cols dcd 2
+; rows dcd 2
+	
     ; area mycode, code, readonly
         ; entry
         ; export reset_handler
 
 ; reset_handler
-; MAX equ 100
+
 ; start
     ; mov r0, #0
+	; mov r8, #0
+	; mov r9, #0
     ; ldr r1, =nums
     ; mov r2, #0
-    ; ldr r3, =MAX
+    ; mov r3, #0
+	; ldr r4, cols
+	; ldr r5, rows
+	
 
-; do1
-    ; ldr r4, [r1, r2]
-    ; add r0, r0, r4
-    ; add r2, r2, #4
-    ; subs r3, #1
-    ; cmp r3, #0
-    ; bgt do1
+; loop1 
+	
+; loop2 
+	; mov r6, r3
+	; mul r6, r4
+	; add r6, r2
+	; mov r7, #4
+	; mul r6, r7
+	; ldr r7, [r1, r6]
+	
+	; mov r0, r7
+	; bl mod5_in_r0
+	; cmp r0, #0
+	; beq add_eq
+	; bne endl2
+; add_eq
+	; add r8, r7
+	; b endl2
+
+	
+; endl2
+
+
+	; add r2, #1
+	; cmp r2, r4
+	; blt loop2
+	; mov r2, #0
+	
+	
+	; add r3, #1
+	; cmp r3, r5
+	; blt loop1
+	
 
 
 ; stop
     ; b stop
+
+	
+; mod5_in_r0 proc
+; begin_mod5
+	; cmp r0, #5
+	; blt end_mod5
+	; sub r0, #5
+	; b begin_mod5
+; end_mod5
+	; bx lr
+	
+	; endp
+	
+	
     ; end
