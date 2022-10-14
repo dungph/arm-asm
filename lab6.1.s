7f49d56bf7a7d75e235bfc01baf4c3f1af590570
Pham Van Dung <dung18j@gmail.com>
2022-10-14 16:47:30 +0700

    lab next

--- lab6.1.s
+++ lab6.1.s
@@ -1,5 +1,62 @@
-; 1.Viết các chương trình so sánh 2 xâu (cài đặt theo độ dài)
-; 2.Viết chương trình đếm số ký tự của một xâu cài đặt theo ký tự kết thúc.
-; 3.Viết chương trình đảo ngược xâu trong hai trường hợp cài đặt theo độ dài và ký tự kết thúc
-; 4.Viết chương trình chuẩn hóa xâu
-; 5.Viết chương trình nối hai xâu.
+; ; 1.Viết các chương trình so sánh 2 xâu (cài đặt theo độ dài)
+; ; 2.Viết chương trình đếm số ký tự của một xâu cài đặt theo ký tự kết thúc.
+; ; 3.Viết chương trình đảo ngược xâu trong hai trường hợp cài đặt theo độ dài và ký tự kết thúc
+; ; 4.Viết chương trình chuẩn hóa xâu
+; ; 5.Viết chương trình nối hai xâu.
+
+
+
+    ; area reset, data, readonly
+         ; dcd 0x20001000
+         ; dcd reset_handler
+	
+	; area data1, data, readonly
+; table1
+	; dcd 3
+	; dcb "cat"
+	
+	; area data2, data, readonly
+; table2
+	; dcd 3
+	; dcb "dog"
+	
+	; area result, data
+	; align
+; match
+	; dcd 0
+
+     ; area mycode, code, readonly
+         ; entry
+         ; export reset_handler
+
+; reset_handler
+	; mov r0, #2
+	; ldr r0, =data1
+	; ldr r1, =data2
+	; ldr r2, =match
+	; ldr r3, [r0], #4
+	; ldr r4, [r1], #4
+	
+	; cmp r3, r4
+	; bne stop
+	; cmp r3, #0
+	; beq equal
+
+; loop 
+	; ldrb r5, [r0], #1
+	; ldrb r6, [r1], #1
+	; cmp r5, r6
+	; bne stop
+	; sub r3, #1
+	; cmp r3, #0
+	; beq stop
+	; b loop
+
+; equal
+	; ldr r7, =match
+	; mov r2, #1
+	; str r2, [r7]
+	
+; stop 	
+	; b stop
+	; end 
---
+++ lab6.2.s
@@ -0,0 +1,44 @@
+; 1.Viết các chương trình so sánh 2 xâu (cài đặt theo độ dài)
+; 2.Viết chương trình đếm số ký tự của một xâu cài đặt theo ký tự kết thúc.
+; 3.Viết chương trình đảo ngược xâu trong hai trường hợp cài đặt theo độ dài và ký tự kết thúc
+; 4.Viết chương trình chuẩn hóa xâu
+; 5.Viết chương trình nối hai xâu.
+
+
+
+    area reset, data, readonly
+         dcd 0x20001000
+         dcd reset_handler
+	
+	area data1, data, readonly
+table1
+	dcb "cat", 0x0D
+
+	area data2, data
+	dcd 0
+
+     area mycode, code, readonly
+         entry
+         export reset_handler
+
+reset_handler
+	ldr r0, =data1
+	mov r1, #0
+
+loop 
+	ldrb r3, [r0], #1
+
+	cmp r3, #13
+
+	beq endloop
+	add r1, #1
+	b loop
+
+
+endloop
+	ldr r4, =data2
+	str r1, [r4]
+
+stop 	
+	b stop
+	end 
